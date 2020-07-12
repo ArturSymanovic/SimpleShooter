1da1c5be4cba43f181ed733f6bb3a53c25b8ede4
Artur Symanovic <artur.symanovic@gmail.com>
2020-07-12 11:24:57 +0100

    Added lose UI widget

--- Source/SimpleShooter/SimpleShooter_PlayerController.cpp
+++ Source/SimpleShooter/SimpleShooter_PlayerController.cpp
@@ -3,10 +3,17 @@
 
 #include "SimpleShooter_PlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void ASimpleShooter_PlayerController::GameHasEnded(class AActor* EndGameFocus /*= nullptr*/, bool bIsWinner /*= false*/)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
+	
+	UUserWidget* UserWidget = CreateWidget(this, LoseScreenClass);
+	if (UserWidget != nullptr)
+	{
+		UserWidget->AddToViewport();
+	}
 
 	GetWorldTimerManager().SetTimer(RestartTimer, this, &ASimpleShooter_PlayerController::RestartLevel, RestartDelay);
 
