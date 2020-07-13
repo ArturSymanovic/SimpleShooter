b2459ec1c1c71ac54185e4c578612a66eb3cefc5
Artur Symanovic <artur.symanovic@gmail.com>
2020-07-13 10:55:17 +0100

    Added end game function to iterate throught all controllers

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -3,6 +3,8 @@
 
 #include "KillEmAllGameMode.h"
 #include "SimpleShooter_PlayerController.h"
+#include "EngineUtils.h"
+#include "ShooterCharacter.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
@@ -11,6 +13,14 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 	ASimpleShooter_PlayerController* PlayerController = Cast<ASimpleShooter_PlayerController>(PawnKilled->GetController());
 	if (PlayerController != nullptr)
 	{
-		PlayerController->GameHasEnded(nullptr, false);
+		EndGame(false);
+	}
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+	for (AController* Controller : TActorRange<AController>(GetWorld()))
+	{
+		Controller->GameHasEnded(Controller->GetPawn(), Controller->IsPlayerController() == bIsPlayerWinner);
 	}
 }
